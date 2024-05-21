4db3085e2a7c39017f98a59051da155a6ca41728
EduardoAragon11 <eduardo.aragon@utec.edu.pe>
2024-05-21 14:35:12 -0500

    salonEtiqueta hecho fixed

--- src/main/java/com/example/hackaton/salonEtiqueta/domain/SalonEtiqueta.java
+++ src/main/java/com/example/hackaton/salonEtiqueta/domain/SalonEtiqueta.java
@@ -1,8 +1,14 @@
 package com.example.hackaton.salonEtiqueta.domain;
 
+import com.example.hackaton.etiqueta.domain.Etiqueta;
+import com.example.hackaton.salon.domain.Salon;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
+@Entity
+@Data
 public class SalonEtiqueta {
     @ManyToOne
     Salon salon;
