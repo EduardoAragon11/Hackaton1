349c0b66d3f9ade25a11a31e043ee7ccaf6d0bd9
Laura <laura.nagamine@utec.edu.pe>
2024-05-21 14:25:08 -0500

    reserva completo

---
+++ src/main/java/com/example/hackaton/reserva/domain/Reserva.java
@@ -0,0 +1,31 @@
+package com.example.hackaton.reserva.domain;
+
+import com.example.hackaton.user.domain.User;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Data
+@Entity
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name ="id", nullable=false)
+    private long idReserva;
+
+    Date fecha;
+
+    Time horaInicio;
+
+    Time horaFin;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    User user;
+
+
+}
