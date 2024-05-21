8bc3cbe81cb5cd56cc352995c2ebed938bb86194
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-05-21 15:12:39 -0500

    cree los endpoints para salon y la asociacion de salon y etiqueta

--- src/main/java/com/example/hackaton/etiqueta/application/EtiquetaController.java
+++ src/main/java/com/example/hackaton/etiqueta/application/EtiquetaController.java
@@ -1,14 +1,4 @@
 package com.example.hackaton.etiqueta.application;
 
-import com.example.hackaton.etiqueta.domain.EtiquetaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/etiqueta")
 public class EtiquetaController {
-
-    @Autowired
-    EtiquetaService etiquetaService;
 }
--- src/main/java/com/example/hackaton/salon/domain/Salon.java
+++ src/main/java/com/example/hackaton/salon/domain/Salon.java
@@ -1,7 +1,11 @@
 package com.example.hackaton.salon.domain;
 
+import com.example.hackaton.etiqueta.domain.Etiqueta;
+import com.example.hackaton.reserva.domain.Reserva;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -19,8 +23,8 @@ public class Salon {
 
     String descripcion;
 
-    @OneToMany(mappedBy = "salon")
-    List<SalonEtiqueta> salonEtiquetas;
+    @ManyToMany(mappedBy = "salones")
+    List<Etiqueta> etiquetas;
 
     @OneToMany(mappedBy = "salon")
     List<Reserva> reservas;
